843f7c21ccf46bc3fb1d07b099bf99d4ff6f81f0
mohammad erfany <98615272+merfdev@users.noreply.github.com>
2024-04-04 12:31:45 +0330

    create first page and complete proj

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,17 @@
+import AllButton from "@/component/module/AllButton";
+import Categories from "@/component/module/Categories";
+import SearchBar from "@/component/module/SearchBar";
+import CarsPage from "@/component/template/CarsPage";
+import carsData from "@/data/carsData";
+
 export default function Home() {
+  const cars = carsData.slice(0, 3);
   return (
     <>
-      <h1>Home</h1>
+      <SearchBar />
+      <Categories />
+      <AllButton />
+      <CarsPage data={cars} />
     </>
   );
 }
